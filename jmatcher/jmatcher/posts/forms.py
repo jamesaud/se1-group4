017041bad1beec202f5fb3aedf6bf6b359d771e5
Pramod Sripada <pramod.sripada@gmail.com>
2017-03-08 11:41:23 -0500

    Posts

--- jmatcher/jmatcher/posts/forms.py
+++ jmatcher/jmatcher/posts/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 
 from .models import Post
+from .models import PostComments
 
 class PostForm(forms.ModelForm):
 	class Meta:
 		model = Post
 		fields = ['description']
+
+class CommentForm(forms.ModelForm):
+	class Meta:
+		model = PostComments
+		fields = ['comment']
--- jmatcher/jmatcher/posts/views.py
+++ jmatcher/jmatcher/posts/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .forms import PostForm
-from .models import Post
+from .forms import PostForm, CommentForm
+from .models import Post, PostComments
 
 def home(request):
     return render(request, 'posts/home.html')
@@ -32,7 +32,29 @@ def get_all_posts(request):
 	return render(request, "posts/showAllPosts.html", context)
 
 def get_post(request, post_id):
+	if request.method == "POST":
+		comment_form = CommentForm(request.POST)
+		if comment_form.is_valid():
+			post = Post.objects.get(pk=post_id)
+			comment = PostComments(commenting_user=request.user, post=post, comment=comment_form.cleaned_data['comment'])
+			comment.save()
 	context = {}
+	comment_form = CommentForm()
 	post_detail = Post.objects.get(pk=post_id)
 	context['post_detail'] = post_detail
+	context['comment_form'] = comment_form
 	return render(request, "posts/showPost.html", context)
+
+from django.http import JsonResponse
+from django.forms.models import model_to_dict
+
+# def post_comments(request, post_id):
+# 	if request.method == "POST":
+# 		comment_form = CommentForm(request.POST)
+# 		if comment_form.is_valid():
+# 			post = Post.objects.get(pk=post_id)
+# 			comment = PostComments(commenting_user=request.user, post=post, comment=comment_form.cleaned_data['comment'])
+# 			comment.save()
+
+
+
