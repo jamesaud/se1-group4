f2907bdacdab7539d86d84f927752a56d167fafb
Pramod Sripada <pramod.sripada@gmail.com>
2017-03-26 15:13:44 -0400

    Sprint3Commit

--- jmatcher/jmatcher/posts/admin.py
+++ jmatcher/jmatcher/posts/admin.py
@@ -4,10 +4,6 @@ from django.contrib import admin
 from .forms import PostForm
 from .models import Post
 from .models import PostComments
-from .models import PostShares
-from .models import PostLikes
 
 admin.site.register(Post)
 admin.site.register(PostComments)
-admin.site.register(PostShares)
-admin.site.register(PostLikes)
--- jmatcher/jmatcher/posts/views.py
+++ jmatcher/jmatcher/posts/views.py
@@ -1,8 +1,11 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from .forms import PostForm
-from .models import Post
-from django.shortcuts import redirect
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, redirect
+from .forms import PostForm, CommentForm
+from .models import Post, PostComments
+try:
+    from django.utils import simplejson as json
+except ImportError:
+    import json
 
 def home(request):
     return render(request, 'posts/home.html')
@@ -32,8 +35,78 @@ def get_all_posts(request):
 	context['posts'] = posts
 	return render(request, "posts/showAllPosts.html", context)
 
+def is_user_post_liked(user, post):
+	if post.likes.all().exists():
+		if post.likes.filter(username=user.username).exists():
+			return True
+		else:
+			return False
+	else:
+		return False
+
+def is_user_post_shared(user, post):
+	if post.shares.all().exists():
+		if post.shares.filter(username=user.username).exists():
+			return True
+		else:
+			return False
+	else:
+		return False
+
 def get_post(request, post_id):
+	if request.method == "POST":
+		comment_form = CommentForm(request.POST)
+		if comment_form.is_valid():
+			post = Post.objects.get(pk=post_id)
+			comment = PostComments(commenting_user=request.user, post=post, comment=comment_form.cleaned_data['comment'])
+			comment.save()
 	context = {}
-	post_detail = Post.objects.get(pk=post_id)
-	context['post_detail'] = post_detail
+	comment_form = CommentForm()
+	post = Post.objects.get(pk=post_id)
+	context['post'] = post
+	context['comment_form'] = comment_form
+
+	if is_user_post_liked(request.user, post):
+		context['like_action'] = "UnLike"
+	else:
+		context['like_action'] = 'Like'
+
+	if is_user_post_shared(request.user, post):
+		context['share_action'] = "Shared"
+	else:
+		context['share_action'] = "Share"
+
 	return render(request, "posts/showPost.html", context)
+
+def like_post(request):
+	if request.method == "POST":
+		post_id = request.POST.get("post_id")
+
+		user = request.user
+		post = Post.objects.get(pk=post_id)
+
+		if is_user_post_liked(user, post):
+			post.likes.remove(user)
+			like_button_text = "Like"
+		else:
+			post.likes.add(user)
+			like_button_text = "UnLike"
+
+		post.save()
+		response = JsonResponse({'like_button_text': like_button_text})
+		return response
+
+def share_post(request):
+	if request.method == "POST":
+		post_id = request.POST.get("post_id")
+
+		user = request.user
+		post = Post.objects.get(pk=post_id)
+
+		if is_user_post_shared(user, post) == False:
+			post.shares.add(user)
+			post.save()
+		
+		share_button_text = "Shared"
+		response = JsonResponse({'share_button_text': share_button_text})
+		return response
