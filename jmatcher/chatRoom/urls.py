0771996c8dfb90a9bb222251cb3a771fb0230bce
Liping Sun <lipsun@indiana.edu>
2017-04-23 18:53:52 -0400

    UI enhancement

---
+++ jmatcher/chatRoom/urls.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(
+        regex=r'^$',
+        view=views.chat,
+        name='chatRoom'
+    ),
+
+    url(r'^new/$', views.new_room, name='new_room'),
+    url(r'^(?P<label>[\w-]{,50})/$', views.chat_room, name='chat_room'),
+
+]
