0de08d6fc7be6e2b6e97e721fd46ea1997fe1a65
jamesaud <jamaudre@indiana.edu>
2017-04-26 13:54:46 -0400

    small fixes

--- jmatcher/posts/forms.py
+++ jmatcher/posts/forms.py
@@ -7,6 +7,9 @@ class PostForm(forms.ModelForm):
 		model = Post
 		fields = ['description']
 		labels = {'description' : ''}
+		widgets = {
+			'description': forms.Textarea(attrs={'cols': 80, 'rows': 2, 'placeholder': "What's on your mind?"}),
+		}
 
 class CommentForm(forms.ModelForm):
 	class Meta:
